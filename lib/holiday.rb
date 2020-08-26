86303c05c2bab0a572a76fdf2bd330f6b646a01a
Valentina E <tiyahelfrih@gmail.com>
2020-08-26 16:10:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,7 +46,7 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
 holiday_hash[:winter].values.flatten
 end
-
+def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do | season, holiday|
      puts "#{season.capitalize}:"
      holiday.each do |holiday, item|
