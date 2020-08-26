e96ff1fddf170c1a9643ad49364dd684eb97d4c0
Valentina E <tiyahelfrih@gmail.com>
2020-08-26 16:14:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,4 +50,20 @@ end
 def all_holidays_with_bbq(holiday_hash)
  
 
+  holiday_hash.each do |season, data|
+    puts "#{season.to_s.capitalize!}:"
+    data.each do |holiday, supply|
+      array = holiday.to_s.split("_")
+      final_holiday = []
+        array.each do |x|
+          final_holiday << x.capitalize!
+        end
+      holiday = final_holiday.join(" ")
+
+      supply = supply.join(", ")
+      puts "  #{holiday}: #{supply}"
+
+    end
+  end
+
 
