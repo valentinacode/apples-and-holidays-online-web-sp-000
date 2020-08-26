809bdd1d4e8000a09aa078c1064f0e293ce0fcf5
Valentina E <tiyahelfrih@gmail.com>
2020-08-26 16:08:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,7 +50,19 @@ end
 def all_supplies_in_holidays(holiday_hash)
 
 def all_holidays_with_bbq(holiday_hash)
+answer = []
+holiday_hash.each do |season, data|
+  data.each do |holiday, value|
+    value.each do |supply|
+     if supply == "BBQ"
+        answer.push(holiday)
+      end
+    end
+  end
+end
+answer
 
+end
 
 
 
